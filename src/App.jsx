af74cb12bbfdfb371cbbcc2c25feda0e5698bb74
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-02-27 12:06:49 +0100

    Create static app jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 function App() {
+	const weekday = new Date().toLocaleDateString("en-US", { weekday: "long" });
+	const numericDay = new Date().toLocaleDateString("en-US", { day: "numeric" });
+	const month = new Date().toLocaleDateString("en-US", { month: "long" });
+	const currentDate = `${weekday}, ${numericDay} ${month}`;
 
-  return (
-    <div>
-      My todo app project
-    </div>
-  )
+	return (
+		<>
+			<div>
+				<h1>My Tasks</h1>
+				<p>{currentDate}</p>
+			</div>
+			<form>
+				<input type="text" placeholder="+ Add your new task..." />
+			</form>
+			<ul>
+				<li>
+					<label>
+						<p>Fake task</p>
+						<input type="checkbox" />
+					</label>
+				</li>
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
