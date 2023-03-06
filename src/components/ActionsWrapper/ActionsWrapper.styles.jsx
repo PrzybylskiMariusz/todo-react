c0e0693694ed48310aea03817bbfd5ad6eff9f9d
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-03-06 19:59:17 +0100

    Change checkbox apperance to custom

--- src/components/ActionsWrapper/ActionsWrapper.styles.jsx
+++ src/components/ActionsWrapper/ActionsWrapper.styles.jsx
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const StyledActionsWrapper = styled.div`
 	display: flex;
+	align-items: center;
 	gap: 0.8rem;
 `;
--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,5 +1,6 @@
 import { StyledListItem } from "./ListItem.styles";
 import { BiTrash } from "react-icons/bi";
+import checkIcon from "../../assets/todo.svg";
 import styled from "styled-components";
 import ActionsWrapper from "../ActionsWrapper";
 import IconWrapper from "../IconWrapper";
@@ -8,19 +9,42 @@ const Paragraph = styled.p`
 	text-decoration: ${({ isDone }) => (isDone ? "line-through" : "none")};
 `;
 
+const StyledCheckbox = styled.input`
+	appearance: none;
+	width: 2.4rem;
+	height: 2.4rem;
+	border: 1px solid ${({ theme }) => theme.grey};
+	border-radius: 50%;
+	cursor: pointer;
+
+	&:checked {
+		background-image: url(${checkIcon});
+		background-size: 120%;
+		background-position: center;
+		border: none;
+	}
+`;
+
+const StyledBiTrash = styled(BiTrash)`
+	width: 2.2rem;
+	height: 2.2rem;
+`;
+
 const ListItem = ({ task, handleChangeTaskStatus, deleteTask }) => {
 	return (
 		<StyledListItem>
 			<Paragraph isDone={task.done}>{task.title}</Paragraph>
 			<ActionsWrapper>
 				<IconWrapper>
-					<BiTrash onClick={() => deleteTask(task.id)} />
+					<StyledBiTrash onClick={() => deleteTask(task.id)} />
 				</IconWrapper>
-				<input
-					type="checkbox"
-					defaultChecked={task.done}
-					onChange={() => handleChangeTaskStatus(task.id)}
-				/>
+				<IconWrapper>
+					<StyledCheckbox
+						type="checkbox"
+						defaultChecked={task.done}
+						onChange={() => handleChangeTaskStatus(task.id)}
+					/>
+				</IconWrapper>
 			</ActionsWrapper>
 		</StyledListItem>
 	);
